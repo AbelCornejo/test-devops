22bb23e0420aaddd9786757d89863e4fe2bfbebe
Abel <acornejorojas20@gmail.com>
2025-09-01 17:45:25 +0000

    Cambios desde la EC2

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-//boton de comprar ahora
+// Cambiar color del header al hacer scroll
+window.addEventListener("scroll", () => {
+  const header = document.getElementById("main-header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+// Botón de comprar ahora
 document.getElementById("btn-comprar").addEventListener("click", () => {
-    alert("Gracias por tu interés, pronto habilitaremos la tienda en línea.");
-})
-
-
-//botones de agregar al carrito
-const addCartButtons = document.querySelectorAll(".add-cart");
-addCartButtons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        alert("Producto agregado al carrito 🐾")
-    });
+  alert("Gracias por tu interés 🐾, pronto habilitaremos la tienda en línea.");
 });
 
-//formulario de contacto
+// Botones de agregar al carrito
+document.querySelectorAll(".add-cart").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Producto agregado al carrito 🐶🐱");
+  });
+});
+
+// Formulario de contacto
 document.getElementById("contact-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Gracias por contactarnos. Te responderemos pronto");
-    e.target.reset();
+  e.preventDefault();
+  alert("Gracias por contactarnos. Te responderemos pronto.");
+  e.target.reset();
 });
+
